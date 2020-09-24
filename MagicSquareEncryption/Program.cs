ce9193a503ba589f37d78d611f9c35654b4df4d1
DenisCo1100100 <kodek_den2001@mail.ru>
2020-09-24 22:02:25 +0300

    Creating project

---
+++ MagicSquareEncryption/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicSquareEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose encryption or decryption (E/D): ");
+            string selectedOperation = Console.ReadLine();
+            VerificatOperations(selectedOperation);
+        }
+
+        private static void VerificatOperations(string operation)
+        {
+            bool isEncryption = operation == "E" ? true : operation == "D" ? false : throw new Exception("Wrong answer");
+
+            if (isEncryption)
+            {
+                Encryption();
+            }
+            else
+            {
+                Decryption();
+            }
+        }
+
+        private static void Encryption()
+        {
+
+        }
+
+        private static void Decryption()
+        {
+
+        }
+    }
+}
