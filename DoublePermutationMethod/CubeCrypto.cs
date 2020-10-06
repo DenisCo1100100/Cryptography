57122de5d0b58f430f7e4196100441355c3233d2
DenisCo1100100 <kodek_den2001@mail.ru>
2020-10-06 14:09:28 +0300

    Added method for encoding and decoding

---
+++ DoublePermutationMethod/CubeCrypto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublePermutationMethod
+{
+    class CubeCrypto
+    {
+        public Cube Cube { get; set; }
+        public CubeCrypto(Cube cube) => Cube = cube;
+
+        public void SetDecoded(string message, int[,] key)
+        {
+            //Кодирование с вводом ключа пользователем
+        }
+
+        public void GetDecoded(string message, int[,] key)
+        {
+
+        }
+
+        public void SetDecoded(string message)
+        {
+            //Кодирование с рандомной генерацией ключа
+        }
+
+        public void SetEncoder(string message, int[,] key)
+        {
+            //Декодирование
+        }
+        public void GetEncoder(string message, int[,] key)
+        {
+            //Декодирование
+        }
+    }
+}
